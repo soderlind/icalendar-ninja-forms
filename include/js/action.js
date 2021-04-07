ce4178d195872fecfaca462abab7487cab18b0d4
Per Søderlind <per@soderlind.no>
2021-04-08 01:37:06 +0200

    Linted

--- include/js/action.js
+++ include/js/action.js
@@ -2,9 +2,15 @@ document.addEventListener(
 	"DOMContentLoaded",
 	() => {
 		const nfRadio = Backbone.Radio;
-		const simpleEventDateChannel = nfRadio.channel("actionSetting-icalendar_date");
-		const simpleEventTimeStartChannel = nfRadio.channel("actionSetting-icalendar_time_start");
-		const simpleEventTimeEndChannel = nfRadio.channel("actionSetting-icalendar_time_end");
+		const simpleEventDateChannel = nfRadio.channel(
+			"actionSetting-icalendar_date",
+		);
+		const simpleEventTimeStartChannel = nfRadio.channel(
+			"actionSetting-icalendar_time_start",
+		);
+		const simpleEventTimeEndChannel = nfRadio.channel(
+			"actionSetting-icalendar_time_end",
+		);
 
 		const SimpleEventsSettings = class extends Marionette.Object {
 			/**
@@ -13,47 +19,55 @@ document.addEventListener(
 			 */
 			initialize() {
 				this.listenTo(simpleEventDateChannel, "update:setting", this.eventDate);
-				this.listenTo(simpleEventTimeStartChannel, "update:setting", this.timeStart);
+				this.listenTo(
+					simpleEventTimeStartChannel,
+					"update:setting",
+					this.timeStart,
+				);
 				this.listenTo(simpleEventTimeEndChannel, "update:setting", this.timeEnd);
 			}
 
 			eventDate(dataModel, settingModel) {
-				if ('undefined' == typeof settingModel) return;
-;
-				var value = dataModel.get('icalendar_date').trim();
+				if ("undefined" === typeof settingModel) {
+					return;
+				}
+				const value = dataModel.get("icalendar_date").trim();
 
-				if (value && (!this.isValidDateFormat(value))) {
+				if (value && !this.isValidDateFormat(value)) {
 					// dataModel.set('icalendar_date', '');
-					dataModel.set(settingModel.get('name'), '');
-					return settingModel.set('warning', icalnfi18n.errorInvalidDateFormat);
+					dataModel.set(settingModel.get("name"), "");
+					return settingModel.set("warning", icalnfi18n.errorInvalidDateFormat);
 				}
 
-				return settingModel.set('warning', false);
+				return settingModel.set("warning", false);
 			}
 
 			timeStart(dataModel, settingModel) {
-				if ('undefined' == typeof settingModel) return;
-
-				var value = dataModel.get('icalendar_time_start').trim();
-
-				if (value && (!this.isValidTimeFormat(value))) {
-
-					return settingModel.set('warning', icalnfi18n.errorInvalidTimeFormat);
+				if ("undefined" === typeof settingModel) {
+					return;
 				}
 
-				return settingModel.set('warning', false);
+				const value = dataModel.get("icalendar_time_start").trim();
+
+				if (value && !this.isValidTimeFormat(value)) {
+					return settingModel.set("warning", icalnfi18n.errorInvalidTimeFormat);
+				}
+
+				return settingModel.set("warning", false);
 			}
 
 			timeEnd(dataModel, settingModel) {
-				if ('undefined' == typeof settingModel) return;
-
-				var value = dataModel.get('icalendar_time_end').trim();
-
-				if (value && (!this.isValidTimeFormat(value))) {
-					return settingModel.set('warning', icalnfi18n.errorInvalidTimeFormat);
+				if ("undefined" === typeof settingModel) {
+					return;
 				}
 
-				return settingModel.set('warning', false);
+				const value = dataModel.get("icalendar_time_end").trim();
+
+				if (value && !this.isValidTimeFormat(value)) {
+					return settingModel.set("warning", icalnfi18n.errorInvalidTimeFormat);
+				}
+
+				return settingModel.set("warning", false);
 			}
 
 			isValidDateFormat(eventDate) {
@@ -62,9 +76,8 @@ document.addEventListener(
 			isValidTimeFormat(eventTime) {
 				return /^\d{2}:\d{2}$/.test(eventTime);
 			}
-
-		}
+		};
 
 		new SimpleEventsSettings();
-	}
+	},
 );
