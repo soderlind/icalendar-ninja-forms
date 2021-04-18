421719a518554952ad3fce199048bfa3c18ca405
Per Søderlind <per@soderlind.no>
2021-04-18 13:01:03 +0200

    1.3.2

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@
  * Plugin URI: https://github.com/soderlind/icalendar-ninja-forms
  * GitHub Plugin URI: https://github.com/soderlind/icalendar-ninja-forms
  * Description: Add an Event to your Ninja Forms.
- * Version:     1.3.1
+ * Version:     1.3.2
  * Author:      Per Søderlind
  * Author URI:  https://soderlind.no
  * Text Domain: icalendar-ninja-forms
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	\wp_die();
 }
 const ICALENDAR_FILE    = __FILE__;
-const ICALENDAR_VERSION = '1.3.1';
+const ICALENDAR_VERSION = '1.3.2';
 
 require_once \plugin_dir_path( ICALENDAR_FILE ) . 'vendor/autoload.php';
 
