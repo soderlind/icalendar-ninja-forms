1313b2e7043c983e59eebced1d05694dd49f6a06
Per Søderlind <per@soderlind.no>
2022-05-06 12:25:28 +0200

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,23 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fbcec6fc7251efc95687b5e1f2fd415cb523562b',
+        'name' => 'soderlind/icalendar-ninja-forms',
+        'dev' => true,
     ),
-    'reference' => NULL,
-    'name' => 'soderlind/icalendar-ninja-forms',
-  ),
-  'versions' => 
-  array (
-    'soderlind/icalendar-ninja-forms' => 
-    array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => NULL,
+    'versions' => array(
+        'soderlind/icalendar-ninja-forms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fbcec6fc7251efc95687b5e1f2fd415cb523562b',
+            'dev_requirement' => false,
+        ),
     ),
-  ),
 );
